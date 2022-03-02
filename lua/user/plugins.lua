d9dc3265914e480bf1f82cd87d08f53002b84f3d
Jesper L. Nielsen <lyager@gmail.com>
2022-03-02 11:19:25 +0100

    - plugin: Removed toggle term, it was messing with my C-j C-k keys

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -67,7 +67,7 @@ return packer.startup(function(use)
   use "akinsho/bufferline.nvim" -- Buffers as tabs in the top?
   use "moll/vim-bbye"
   use "nvim-lualine/lualine.nvim"
-  use "akinsho/toggleterm.nvim"
+  --use "akinsho/toggleterm.nvim"
   use "ahmedkhalf/project.nvim"
   use "lewis6991/impatient.nvim"
   use "lukas-reineke/indent-blankline.nvim"
