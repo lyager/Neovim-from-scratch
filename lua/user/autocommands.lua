69c710aa79e793b001460d891a336877a183a0f4
Jesper L. Nielsen <lyager@gmail.com>
2022-04-25 10:18:18 +0200

    Set javascript indentation locally.

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -2,6 +2,7 @@ vim.cmd [[
   augroup _general_settings
     autocmd!
     autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
