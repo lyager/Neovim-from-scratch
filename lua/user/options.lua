17c84cf15f888e2f43bf56857ca6f516bd14844b
Jesper L. Nielsen <lyager@users.noreply.github.com>
2023-12-05 09:36:13 +0100

    fix(options): Set number = true, set cursorline = true

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -24,8 +24,8 @@ local options = {
   expandtab = true,                        -- convert tabs to spaces
   shiftwidth = 4,                          -- the number of spaces inserted for each indentation
   tabstop = 4,                             -- insert 4 spaces for a tab
-  cursorline = false,                       -- highlight the current line
-  number = false,                           -- set numbered lines
+  cursorline = true,                      -- highlight the current line
+  number = true,                          -- set numbered lines
   relativenumber = false,                  -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
 
