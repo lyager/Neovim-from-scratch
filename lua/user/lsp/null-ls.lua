fb2de376ef71d962ae932e1380deaf2f51d74125
Jesper L. Nielsen <lyager@users.noreply.github.com>
2023-12-20 11:51:55 +0100

    fix(nulls): prefer eslint to prettier
    As eslint usually would just use the prettier plugin

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,7 +11,9 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        formatting.eslint,
+        diagnostics.eslint,
+		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
         diagnostics.phpcs.with({ extra_args = { "--standard=PSR2" } })
