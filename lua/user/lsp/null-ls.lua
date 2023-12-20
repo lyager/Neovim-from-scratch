cb831dd3f0877e84c3f8a1648b0db1762f363d8e
Jesper L. Nielsen <lyager@users.noreply.github.com>
2023-12-20 13:12:16 +0100

    feat(nullls): format on save

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,12 +11,27 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-        formatting.eslint,
-        diagnostics.eslint,
+		formatting.prettier,
+		diagnostics.eslint,
 		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-        diagnostics.phpcs.with({ extra_args = { "--standard=PSR2" } })
-    -- diagnostics.flake8
+		diagnostics.phpcs.with({ extra_args = { "--standard=PSR2" } }),
+		-- diagnostics.flake8
 	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+					--vim.lsp.buf.formatting_sync()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
