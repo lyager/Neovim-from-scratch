97467e30b8c213378e7f5dce1d2ea736b10fefa6
Jesper L. Nielsen <lyager@users.noreply.github.com>
2024-02-12 13:04:13 +0100

    fix: indentation defaults and format

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,10 +1,10 @@
-vim.cmd [[
+vim.cmd([[
   augroup _general_settings
     autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
+    autocmd FileType python setlocal tabstop=4 shiftwidth=4 expandtab
     autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab
-    autocmd FileType python setlocal ts=4 sw=4 et
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
   augroup end
@@ -23,14 +23,14 @@ vim.cmd [[
 
   augroup _auto_resize
     autocmd!
-    autocmd VimResized * tabdo wincmd = 
+    autocmd VimResized * tabdo wincmd =
   augroup end
 
   augroup _alpha
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
-]]
+]])
 
 -- Autoformat
 -- augroup _lsp
