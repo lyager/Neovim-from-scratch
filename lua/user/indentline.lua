d540219ffa59e82b190c1781d3ef765b10f037d2
Jesper L. Nielsen <lyager@users.noreply.github.com>
2024-03-06 15:38:08 +0100

    fix: visually simpler indentline

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,16 +1,25 @@
-local status_ok, ibl = pcall(require, "ibl")
+local status_ok, indent = pcall(require, "ibl")
 if not status_ok then
+	vim.notify("indent_blankline not found")
 	return
 end
+
 local highlight = {
-	"CursorColumn",
-	"Whitespace",
+	"Grey",
 }
-require("ibl").setup({
-	indent = { highlight = highlight, char = "" },
-	whitespace = {
-		highlight = highlight,
-		remove_blankline_trail = false,
-	},
+
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "Grey", { fg = "#4b5263" })
+end)
+
+indent.setup({
+	enabled = true,
 	scope = { enabled = false },
+	indent = {
+		char = "│",
+		highlight = highlight,
+	},
 })
