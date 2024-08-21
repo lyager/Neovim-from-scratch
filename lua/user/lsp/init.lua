48174ddc925d50c9599a52363f04a593353e7f74
Jesper L. Nielsen <lyager@users.noreply.github.com>
2024-08-21 14:28:33 +0200

    chore: set lsp log level to warn

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,7 +3,7 @@ if not status_ok then
 	return
 end
 
---vim.lsp.set_log_level("debug")
+vim.lsp.set_log_level("warn")
 require("user.lsp.mason")
 require("user.lsp.handlers").setup()
 require("user.lsp.null-ls")
