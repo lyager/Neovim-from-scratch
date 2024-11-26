e9a042bf924ccf44767655a949a82703afe0b136
Jesper L. Nielsen <lyager@users.noreply.github.com>
2024-11-26 09:30:21 +0100

    fix: remove eslint for null-ls, use eslint-LSP instead

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,7 +12,7 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier,
-		diagnostics.eslint,
+		--diagnostics.eslint,
 		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
