c5b520380291ae13164190ed7858b70a1576bd13
Jesper L. Nielsen <lyager@users.noreply.github.com>
2025-06-19 13:39:38 +0200

    chore(handlers): removed more deprecated things

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -11,22 +11,15 @@ M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
-    local signs = {
-
-        { name = "DiagnosticSignError", text = "" },
-        { name = "DiagnosticSignWarn", text = "" },
-        { name = "DiagnosticSignHint", text = "" },
-        { name = "DiagnosticSignInfo", text = "" },
-    }
-
-    for _, sign in ipairs(signs) do
-        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-    end
-
     local config = {
-        virtual_text = false, -- disable virtual text
+        -- virtual_text = false, -- disable virtual text
         signs = {
-            active = signs,   -- show signs
+            text = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = "",
+            },
         },
         update_in_insert = true,
         underline = true,
@@ -42,14 +35,6 @@ M.setup = function()
     }
 
     vim.diagnostic.config(config)
-
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = "rounded",
-    })
-
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-        border = "rounded",
-    })
 end
 
 local function lsp_keymaps(bufnr)
